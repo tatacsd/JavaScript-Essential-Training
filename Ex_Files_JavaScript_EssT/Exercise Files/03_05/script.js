e79f187dbecfb63dc666fd993c4a651d9fa8c72f
Thays <thayscasado@hotmail.com>
2020-12-11 02:37:47 -0800

    Class 03_05 completed >> logical operators

--- Ex_Files_JavaScript_EssT/Exercise Files/03_05/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_05/script.js
@@ -8,4 +8,25 @@ if ( a == b ) {
     theNumbersMatch = false;
 }
 
-console.log(theNumbersMatch);
+console.log("The numbers match: " + theNumbersMatch);
+// Identical ===
+
+var a = 5;
+var b = "5";
+var theNumbersMatch
+
+if ( a === b ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log("The numbers match: " + theNumbersMatch);
+
+// != not equal (bang)
+
+if(a == false) {}; // longhand
+if(a != true) {}; // Reverse psycology
+if(!a) {}; // Shorthand. Literally "NOT a".
+
+
