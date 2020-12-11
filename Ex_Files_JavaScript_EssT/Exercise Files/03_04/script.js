1727c1a3f8c7a94dfcc08d6812df2d87390a7c31
Thays <thayscasado@hotmail.com>
2020-12-11 02:29:36 -0800

    class 03_04 completed >> NaN

--- Ex_Files_JavaScript_EssT/Exercise Files/03_04/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_04/script.js
@@ -1,3 +1,24 @@
 var a = 5;
 var b = 4;
 var sum = a + b;
+
+var humanSum = "The value of a: " + a + ". The value of b: " + b +". The sum of a and b: " + sum + ".";
+console.log(humanSum);
+
+// concatenation
+
+var a = 5;
+var b = "4";
+var sum = a + b; //54
+
+console.log(sum);
+
+// Error
+
+var a = 5;
+var b = "duck";
+var sum = a - b; // NaN -> Not a number (one or more is a string)
+
+console.log(sum);
+
+
