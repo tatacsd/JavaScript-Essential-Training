556a0f7df3e5546f9e60bcfdf949688b622cfca8
Thays <thayscasado@hotmail.com>
2020-12-11 01:35:20 -0800

    class 02_02 completed

---
+++ Ex_Files_JavaScript_EssT/Exercise Files/02_02/02_02.js
@@ -0,0 +1,13 @@
+
+alert("I love code");
+
+var date = new Date();
+alert("today is " + date);
+
+console.log(date);
+
+
+// document.body.innerHTML = "<h1> The date today is" + date + " </h1>";
+// document.body.innerHTML = "<h1> The date today is" + date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear() + " </h1>";
+// document.body.innerHTML = "<h1> The date today is " + date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear() + " </h1>";
+// document.body.innerHTML = "<h1> The date today is " + (date.getMonth()+1) + "/" + date.getDay() + "/" + date.getFullYear() + " </h1>";
