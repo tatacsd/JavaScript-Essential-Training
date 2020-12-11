ddf98561cb41c649251de14c9bcfb4393cda7cec
Thays <thayscasado@hotmail.com>
2020-12-11 02:23:16 -0800

    class 03_03 completed >> math operators >> increment

--- Ex_Files_JavaScript_EssT/Exercise Files/03_03/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_03/script.js
@@ -6,3 +6,21 @@ var justAnotherVariable;
 
 // Try this in your console:
 // console.log(typeof insertVariableName);
+
+// Shorthand math: +=, -= *= /=
+var a = 5;
+
+a = a + 4; //longform version
+a += 4; //shorthand version
+
+// Unary operator: ++ --
+var a = 5;
+
+a = a - 1; //longform version
+a--;; //shorthand version
+
+// PAY ATTENTIO HERE
+
+a++; // grab the original value, THEN add 1 to it.
+++a; // add one to the value
+
