8638d02b278146ff3df034c4ddd990dc7c6f7381
Thays <thayscasado@hotmail.com>
2020-12-11 02:52:21 -0800

    class 03_06 completed >> advanced logic operator >> XOR >> Ternary Operator

--- Ex_Files_JavaScript_EssT/Exercise Files/03_05/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_05/script.js
@@ -29,4 +29,33 @@ if(a == false) {}; // longhand
 if(a != true) {}; // Reverse psycology
 if(!a) {}; // Shorthand. Literally "NOT a".
 
+// ADVANCED CONDITION
+
+//And = both need to be true to whole be true
+if(a && b) {};
+//OR = one, another or both.
+if(a || b) {};
+
+//XOR -> does not exist in Js need to be created
+//If a equals b AND c equals d:
+if(a == b && c == d) {}
+//If a equals b OR c equals d (or both is true):
+if(a == b || c == d) {}
+//If a equals b XOR c equals d (NOT BOTH CAN BE TRUE):
+if((a == b || c == d) && ((a == b) != (c == d))) {}
+// "either a or b but not a and b"
+
+
+// Ternary Operator
+// condition ?(if is true) statement :(else) statement
+a == b ? console.log("Match") : console.log("No match"); //Shorthand
+
+// longhand
+if (a == b){
+    console.log("Match");
+} else {
+    console.log("No match");
+}
+
+
 
