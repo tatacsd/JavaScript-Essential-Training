0e3865654517730bb0046a9176e01bc27236c57e
Thays <thayscasado@hotmail.com>
2020-12-11 02:16:14 -0800

    class 03_02 completed >> data type

--- Ex_Files_JavaScript_EssT/Exercise Files/03_02/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_02/script.js
@@ -1,8 +1,8 @@
 var negInteger = -3.14159265359;
 var escQuote = "Quotes can also be \"escaped\".";
 var theSunIsWarm = true;
-var emptyInside = null;
-var justAnotherVariable;
+var emptyInside = null; //empty
+var justAnotherVariable; //undefined
 
 // Try this in your console:
 // console.log(typeof insertVariableName);
